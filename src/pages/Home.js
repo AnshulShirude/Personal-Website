eccad2bd20d1c494e2878c183ce031fda2820b47
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2023-05-11 13:09:24 -0400

    Moved the slideshow to the right and added in text blurb

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,40 +10,51 @@ import pic4 from '../images/anshulPic4.jpg'
 const Home = () => {
   return (
     <div className="outer-container">
-    <div className="inner-container">
-    <div className="slide-container">
-      <Slide autoplay={false} duration={5000}>
-        <div className="each-slide-effect">
-          <div
-            style={{
-              backgroundImage: `url(${pic1})`
-            }}
-          />
+      <div className="inner-container">
+        <div className="wave-container">
+          <div className="hand-wave">
+            👋
+          </div>
+          <div className="name">
+            Hello, I am Anshul Shirude
+          </div>
+          <div className="description">
+            I am a junior at Northeastern University studying <br /> Computer Science. My interests lie in data visualization, <br /> machine learning, and full stack development.
+          </div>
         </div>
-        <div className="each-slide-effect">
-          <div
-            style={{
-              backgroundImage: `url(${pic2})`
-            }}
-          />
+        <div className="slide-container">
+          <Slide autoplay={false} duration={5000}>
+            <div className="each-slide-effect">
+              <div
+                style={{
+                  backgroundImage: `url(${pic1})`
+                }}
+              />
+            </div>
+            <div className="each-slide-effect">
+              <div
+                style={{
+                  backgroundImage: `url(${pic2})`
+                }}
+              />
+            </div>
+            <div className="each-slide-effect">
+              <div
+                style={{
+                  backgroundImage: `url(${pic3})`
+                }}
+              />
+            </div>
+            <div className="each-slide-effect">
+              <div
+                style={{
+                  backgroundImage: `url(${pic4})`
+                }}
+              />
+            </div>
+          </Slide>
         </div>
-        <div className="each-slide-effect">
-          <div
-            style={{
-              backgroundImage: `url(${pic3})`
-            }}
-          />
-        </div>
-        <div className="each-slide-effect">
-          <div
-            style={{
-              backgroundImage: `url(${pic4})`
-            }}
-          />
-        </div>
-      </Slide>
-    </div>
-    </div>
+      </div>
     </div>
   );
 };
