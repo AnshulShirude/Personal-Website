f0e49a6cd7b66f631209fa3cd41c0a7ecb937247
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2023-05-19 23:31:45 -0400

    fixed all of the visual aids for the clubs

--- src/pages/Clubs.js
+++ src/pages/Clubs.js
@@ -6,19 +6,21 @@ const Clubs = () => {
     return (
         <div className="outer-container">
             <div className="inner-container">
-                <h1> Clubs @ Northeastern</h1>
+                <h1>⭐️ Clubs @ Northeastern ⭐️</h1>
                 <div>
                     <div class="company">
                         <img src="https://nutamid.org/wp-content/uploads/2021/09/Frame-4-5.png" alt="Logo" class="logo"/>
                     </div>
                     <h4>December 2021 - April 2023</h4>
                     <ul>
-                        <li>TAMID is a national consulting club which connects undergraduate students with startups within the Israeli economy.</li>
-                        <li>Revamped components using Tailwind CSS, improved conditional routing, and fixed security leakages for my first deliverable GiftPicker.</li>
-                        <li>Member of the full-stack team responsible for conceptualizing and developing a "Secret-Santa" style gift exchange integrated with k-means clustering algorithm to efficiently match users.</li>
-                        <li>Taught Tech Consulting Foundations to a group of 18 students, a course for students to learn HTML, CSS, JS, and React frameworks before joining Tech Consulting.</li>
-                        <li>Completed data visualization work using Matplotlib to generate a responsive map related to e-scooter rider safety. Created an effective visual analysis in the form of a chart to provide each scooter rider a score based upon multiple factors with VComm.</li>
-                        <li>Worked in developing the Contact page for Invictuss and assisted peers while serving as a mentor within the track. Triggered an API call to get in-touch with the owners directly from the website.</li>
+                        <li><a href="https://nutamid.org/" target="_blank" class="link-black">TAMID</a> is a national consulting club which connects undergraduate students with startups within the Israeli economy.</li>
+                        <li><strong>Revamped components</strong> using <strong>Tailwind CSS, improved conditional routing, and fixed security leakages</strong> for my first deliverable <a href="https://giftpicker.io/" target="_blank" class="link-black">GiftPicker.</a ></li>
+                        <li>Member of the <strong>full-stack team</strong> responsible for conceptualizing and developing a "Secret-Santa" 🎅 style gift exchange 🎁 integrated with <strong>k-means clustering algorithm</strong> to efficiently match users.</li>
+                        <li><strong>Taught <a href="https://nutamid.org/tech-consulting/" target="_blank" class="link-black">Tech Consulting Foundations</a></strong> to a group of 18 students, a course for students to learn <strong>HTML, CSS, JS, and React frameworks</strong> before joining <a href="https://nutamid.org/tech-consulting/" target="_blank" class="link-black">Tech Consulting</a>.</li>
+                        <li>Completed <strong>data visualization</strong> work using <a href="https://matplotlib.org/" target="_blank" class="link-black">Matplotlib</a> to <strong>generate a responsive map</strong> 🗺️ related to e-scooter 🛵 rider safety.</li>
+                        <li>Created an <strong>effective visual analysis</strong> in the form of a chart to provide each scooter rider a score based upon multiple factors with <a href="https://nutamid.org/tech-consulting/" target="_blank" class="link-black">VComm</a>.</li>
+                        <li>Worked in <strong> developing the Contact page</strong> for <a href="https://invictuss.vercel.app/" target="_blank" class="link-black">Invictuss</a> and assisted peers ✏️ while serving as a <strong>mentor</strong> within the track.</li>
+                        <li><strong>Triggered an API call</strong> to get in-touch with the owners directly from the website.</li>
                     </ul>
                 </div>
                 <div>
@@ -27,10 +29,11 @@ const Clubs = () => {
                     </div>
                     <h4>April 2023 - Present</h4>
                     <ul>
-                        <li>IDEA is Northeastern's student-led venture accelerator that fosters the development of entrepreneurs in the Northeastern community through the educational experience of developing a business from concept to launch.</li>
-                        <li>Software Development lead for my venture Commently, a platform which creates a visual report based upon analyzing customer ratings and reviews using Natural Language Processing.</li>
+                        <li><a href="https://www.northeastern.edu/idea/" target="_blank" class="link-black">IDEA</a> is Northeastern's student-led venture accelerator that fosters the development of entrepreneurs in the Northeastern community through the educational experience of developing a business from concept to launch.</li>
+                        <li>Software Development Lead for my venture <strong><a href="http://surl.li/hdrkq" target="_blank" class="link-black">Commently</a></strong>, a platform which creates a <strong>visual report based upon analyzing customer ratings and reviews</strong> using Natural Language Processing.</li>
                         <li>Fully ideated and presented to the judges the value proposition, market research, and the affiliate model for the venture.</li>
-                        <li>Development for the venture in progress (progress so far): Integrated the back-end to the front-end using React to access the django API and also built a search optimization filter.</li>
+                        <li>Development for the venture in progress : <strong>Integrated the back-end to the front-end</strong> using React to access the <strong> django API</strong> and also built a <strong>search optimization filter</strong>.</li>
+                        <li><em><a href="https://github.com/AnshulShirude/Commently" target="_blank" class="link-black">GitHub</a> code can be made available upon request.</em></li>
                     </ul>
                 </div>
                 <div> 
@@ -39,8 +42,8 @@ const Clubs = () => {
                     </div>
                     <h4>January 2023 - April 2023</h4>
                     <ul>
-                        <li>Entrepreneur's Club is Northeastern's largest student-led organization on campus with over 2000 active members and more than 30 events each semester across 4 programs.</li>
-                        <li>Took part in the Husky Startup Challenge to develop my venture Commently, the largest flagship program which provides early stage startups with the knowledge, resources, and mentorship they need to flourish.</li>
+                        <li><a href="https://www.nuentrepreneursclub.com/" target="_blank" class="link-black">Entrepreneur's Club</a> is Northeastern's largest student-led organization on campus with over 2000 active members and more than 30 events each semester across 4 programs.</li>
+                        <li>Took part in the <a href="https://www.nuentrepreneursclub.com/hsc" target="_blank" class="link-black">Husky Startup Challenge</a> to develop my <strong>venture Commently</strong>, the largest flagship program which provides early stage startups with the knowledge, resources, and mentorship they need to flourish.</li>
                     </ul>
                 </div>
                 <div> 
@@ -49,9 +52,10 @@ const Clubs = () => {
                     </div>
                     <h4>September 2022 - December 2022</h4>
                     <ul>
-                        <li>Disrupt is a community to drive education, advancement and engagement in the space of Financial Technology (Fintech) at Northeastern.</li>
-                        <li>Fully developed a venture idea, Azura, in relation to investing based upon stock market trends. Completed user research, market analysis, SWOT analysis, and developed a revenue model.</li>
-                        <li>Placed 3rd place amongst a cohort of 15+ teams.</li>
+                        <li><a href="https://www.disruptneu.com/" target="_blank" class="link-black">Disrupt</a> is a community to drive education, advancement and engagement in the space of Financial Technology (Fintech) at Northeastern.</li>
+                        <li>Fully developed a venture idea, <strong><a href="https://pitch.com/public/c9393995-caac-48c3-8cbb-935aaf3cdf73" target="_blank" class="link-black">Azura</a></strong>, in relation to investing based upon stock 📈 market trends.</li>
+                        <li>Completed <strong> user research, market analysis, SWOT analysis, and developed a revenue model</strong>.</li>
+                        <li>Placed <strong>3rd place</strong> 🏆 amongst a cohort of 15+ teams.</li>
                     </ul>
                 </div>                
             </div>
