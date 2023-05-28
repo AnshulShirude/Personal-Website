bc834604880df0f49bc4e9a321789351ce6c40e6
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2023-05-28 18:20:52 -0400

    Done

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 export default function Navbar() {
-  const path = window.location.pathname;
+  const outerContainerRef = useRef(null);
+  const innerContainerRef = useRef(null);
+  const siteTitleRef = useRef(null);
+  const [marginLeft, setMarginLeft] = useState(150);
+  const [marginRight, setMarginRight] = useState(9);
+
+  useEffect(() => {
+    const outerContainer = outerContainerRef.current;
+    const innerContainer = innerContainerRef.current;
+    const siteTitle = siteTitleRef.current;
+
+    const outerRect = outerContainer.getBoundingClientRect();
+    const innerRect = innerContainer.getBoundingClientRect();
+
+    const marginLeftValue = window.innerWidth < 768 ? 0 : 150;
+    setMarginLeft(marginLeftValue);
+
+    const marginRightValue = window.innerWidth < 768 ? 0 : 9;
+    setMarginRight(marginRightValue);
+
+    // Update marginLeft and marginRight values when the window is resized
+    const handleResize = () => {
+      const newMarginleftValue = window.innerWidth < 768 ? 0 : 9;
+      setMarginLeft(newMarginleftValue);  
+
+      const newMarginRightValue = window.innerWidth < 768 ? 0 : 9;
+      setMarginRight(newMarginRightValue);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
-    <nav className="nav">
-      <a href="/" className="site-title">
+    <nav className="nav" ref={outerContainerRef}>
+      <a href="/" className="site-title" ref={siteTitleRef} style={{ marginLeft: `${marginLeft}px` }}>
         Anshul
       </a>
-      <ul>
+      <ul style={{ marginRight: `${marginRight}rem` }} ref={innerContainerRef}>
         <CustomLink href="/aboutme">About Me</CustomLink>
         <CustomLink href="/clubs">Clubs</CustomLink>
         <CustomLink href="/projects">Projects</CustomLink>
